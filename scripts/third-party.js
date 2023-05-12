0ed8b6399a56af59cc06c1b75ad087db8ec157bf
Marquise Rosier <mrosier@adobe.com>
2023-05-12 08:37:41 -0700

    fix(partytown): wrong link

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -1,4 +1,4 @@
-const ECHARTS = 'https://www.jsdelivr.com/package/npm/echarts';
+const ECHARTS = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
 
 function createInlineScriptHTML(innerHTML, parent) {
   const script = document.createElement('script');
